913cdd58b9da7bdd86ed6401ec555895e905cc7b
Taewan Lim <taewan.seoul@gmail.com>
2022-10-16 18:26:02 +0900

    ✨ feat: set up users endpoint

--- src/api/routes/users.ts
+++ src/api/routes/users.ts
@@ -1,14 +1,25 @@
 import { Router } from "express";
 
-import catchAsync from "../../utils/asyncHandler";
+import Joi from "joi";
+
+import verifyToken from "../middlewares/verifyToken";
+import validateSchema from "../middlewares/validateSchema";
 
 const route = Router();
 
-const userRouter = (app: Router) => {
-  app.use("/users", route);
+const usersRouter = (app: Router) => {
+  app.use("/users", verifyToken, route);
 
   route.get(
     "/:user_id/data",
-    catchAsync(async (req, res, next) => {}),
+    validateSchema(
+      Joi.object({
+        user_id: Joi.string().regex(/^[0-9a-fA-F]{24}$}/),
+      }),
+      "params",
+    ),
+    (req, res, next) => {},
   );
 };
+
+export default usersRouter;
