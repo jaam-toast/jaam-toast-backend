93a0214f5ca6117d3bf798c7b8dc12b6de1b0479
Taewan Lim <taewan.seoul@gmail.com>
2022-10-22 21:44:50 +0900

    ♻️ refactor: modify errorHandler due to error regarding view engine

--- src/api/middlewares/errorHandler.ts
+++ src/api/middlewares/errorHandler.ts
@@ -1,11 +1,18 @@
 import { ErrorRequestHandler } from "express";
+import Logger from "../../loaders/logger";
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
+  Logger.error(
+    `${err.status || 500} - ${err.message} - ${req.originalUrl} - ${
+      req.method
+    } - ${req.ip}`,
+  );
+
   res.status(err.status || 500);
-  res.render("error");
+  res.json(err);
 };
 
 export default errorHandler;
