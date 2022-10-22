a48158155c889b2c6d72f16ce6b52608c925eacb
Taewan Lim <taewan.seoul@gmail.com>
2022-10-22 21:40:28 +0900

    ✨ feat: set up axios instances for github oauth and api requests, respectively

---
+++ src/api/github/oauth.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosInstance } from "axios";
+
+import Config from "../../config";
+
+type GithubToken = {
+  access_token: string;
+  scope: string;
+  token_type: string;
+};
+
+const GithubOauth: AxiosInstance = axios.create({
+  baseURL: "https://github.com",
+  timeout: 2500,
+  headers: {
+    Accept: "application/json",
+  },
+});
+
+export const getGithubToken = async (code: string) => {
+  const { data } = await GithubOauth.post<GithubToken>(
+    "/login/oauth/access_token?",
+    null,
+    {
+      params: {
+        client_id: Config.CLIENT_ID,
+        client_secret: Config.CLIENT_SECRET,
+        code,
+      },
+    },
+  );
+
+  return data.access_token;
+};
