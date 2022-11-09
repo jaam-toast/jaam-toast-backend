a4d59894b4cfc3d6eed85ef3920a9a88c081cd9d
Taewan Lim <taewan.seoul@gmail.com>
2022-11-10 01:19:25 +0900

    ✨ feat: modify errorHandler due to custom DeploymentError

--- src/api/middlewares/errorHandler.ts
+++ src/api/middlewares/errorHandler.ts
@@ -5,11 +5,19 @@ const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  Logger.error(
-    `${err.status || 500} - ${err.message} - ${req.originalUrl} - ${
-      req.method
-    } - ${req.ip}`,
-  );
+  if (err.name === "DeploymentError") {
+    Logger.error(
+      `${err.status || 500} - ${err.code} - ${err.message} - ${
+        req.originalUrl
+      } - ${req.method} - ${req.ip}`,
+    );
+  } else {
+    Logger.error(
+      `${err.status || 500} - ${err.message} - ${req.originalUrl} - ${
+        req.method
+      } - ${req.ip}`,
+    );
+  }
 
   res.status((err.status as number) || 500);
   res.json(err);
