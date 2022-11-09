059b1d689c86cfe04b688b075e1db3fea7cf2254
Taewan Lim <taewan.seoul@gmail.com>
2022-11-10 00:43:30 +0900

    ✨ feat: create createDeployment build-util function

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,6 +9,10 @@ const Config = {
   JWT_SECRET: process.env.JWT_SECRET_KEY,
   CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+  INSTANCE_TYPE: process.env.AWS_INSTANCE_TYPE,
+  AMI_ID: process.env.AWS_AMI_ID,
+  KEY_PAIR_NAME: process.env.AWS_KEY_PAIR_NAME,
+  IAM_INSTANCE_PROFILE: process.env.AWS_IAM_INSTANCE_PROFILE,
 };
 
 export default Config;
