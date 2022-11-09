c4a107ec0028e7f116357fdcd1b1b645d3d0cb2d
Taewan Lim <taewan.seoul@gmail.com>
2022-11-10 00:43:57 +0900

    ✨ feat: create types for deployment

--- src/types/custom/index.d.ts
+++ src/types/custom/index.d.ts
@@ -4,3 +4,41 @@ export type User = {
   userImage?: string;
   githubAccessToken?: string;
 };
+
+export interface ClientOptions {
+  remoteUrl: string;
+  repoName: string;
+}
+
+export interface Env {
+  key: string;
+  value: string;
+}
+
+export interface GitMetadata {
+  commitAuthorName?: string | undefined;
+  commitMessage?: string | undefined;
+  remoteUrl: string;
+}
+
+export interface DeploymentOptions {
+  nodeVersion: string;
+  installCommand?: string;
+  buildCommand?: string;
+  envList?: Env[];
+  gitMetadata?: GitMetadata;
+}
+
+export interface InstanceParams {
+  ImageId: string | undefined;
+  InstanceType: string | undefined;
+  KeyName: string | undefined;
+  MinCount: number;
+  MaxCount: number;
+  UserData: string;
+  IamInstanceProfile: {
+    Arn: string | undefined;
+  };
+}
+
+export interface RepoBuildOptions extends ClientOptions, DeploymentOptions {}
