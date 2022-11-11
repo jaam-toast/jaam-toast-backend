353fcf2ed9cf8d2d1c82c432b302c3abce844073
Taewan Lim <taewan.seoul@gmail.com>
2022-11-11 20:44:42 +0900

    ✨ feat: create route53_describerecord

---
+++ src/deploy/aws/route53_describerecord.ts
@@ -0,0 +1,34 @@
+import { GetChangeCommand } from "@aws-sdk/client-route-53";
+
+import { createDeploymentDebug } from "../../utils/createDebug";
+import { DeploymentError } from "../../utils/errors";
+
+import route53Client from "./libs/route53Client";
+
+const describeRecord = async (Id: string) => {
+  const debug = createDeploymentDebug(true);
+
+  try {
+    const command = new GetChangeCommand({ Id });
+
+    const data = await route53Client.send(command);
+
+    if (data.ChangeInfo) {
+      const { Status } = data.ChangeInfo;
+
+      const recordStatus = Status;
+
+      return recordStatus;
+    }
+  } catch (err) {
+    debug(
+      `Error: An unexpected error occurred during GetChangeCommand - ${err}`,
+    );
+    throw new DeploymentError({
+      code: "route53Client_GetChangeCommand",
+      message: "GetChangeCommand didn't work as expected",
+    });
+  }
+};
+
+export default describeRecord;
