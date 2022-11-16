98e9ef17b8720f2bd75d4c8b2a05ba0a7d5e77b1
Taewan Lim <taewan.seoul@gmail.com>
2022-11-16 13:43:07 +0900

    ✨ feat: set up cloudwatch DescribeLogStreamsCommand function

---
+++ src/deploy/aws/cwl_describelogstreams.ts
@@ -0,0 +1,41 @@
+import {
+  DescribeLogStreamsCommand,
+  LogStream,
+} from "@aws-sdk/client-cloudwatch-logs";
+
+import Config from "../../config";
+import cwlClient from "./libs/cloudWatchLogsClient";
+
+import { DeploymentError } from "../../utils/errors";
+import { createDeploymentDebug } from "../../utils/createDebug";
+
+const describeLogStreams = async (instanceId: string) => {
+  const debug = createDeploymentDebug(Config.CLIENT_OPTIONS.debug);
+
+  const logStreamsParams = {
+    logGroupName: "user-data.log",
+    logStreamNamePrefix: instanceId,
+  };
+
+  try {
+    const command = new DescribeLogStreamsCommand(logStreamsParams);
+
+    const data = await cwlClient.send(command);
+
+    if (data.logStreams) {
+      const logStream: LogStream = data.logStreams[0];
+
+      return logStream;
+    }
+  } catch (err) {
+    debug(
+      `Error: An unexpected error occurred during DescribeLogStreamsCommand - ${err}`,
+    );
+    throw new DeploymentError({
+      code: "cloudWatchLogsClient_DescribeLogStreamsCommand",
+      message: "DescribeLogStreamsCommand didn't work as expected",
+    });
+  }
+};
+
+export default describeLogStreams;
