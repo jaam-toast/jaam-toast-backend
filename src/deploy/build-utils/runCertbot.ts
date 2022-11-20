6c8de223914cb0024ecddd483e675644ea36eeee
Taewan Lim <taewan.seoul@gmail.com>
2022-11-20 19:09:02 +0900

    ✨ feat: modify runCertbot to await setInterval to make deployment logic happens in sequence

--- src/deploy/build-utils/runCertbot.ts
+++ src/deploy/build-utils/runCertbot.ts
@@ -1,57 +1,65 @@
 import { InstanceStateName } from "@aws-sdk/client-ec2";
 
 import Config from "../../config";
-import describeInstanceIp from "../aws/ec2_describeinstances";
-import describeRecord from "../aws/route53_describerecord";
 import runCertbotCommands from "../cli/runCertbotCommands";
+import getRecordInstanceStatus from "./getRecordInstanceStatus";
 
-import { createDeploymentDebug } from "../../utils/createDebug";
+import { createCertbotDebug } from "../../utils/createDebug";
 import { DeploymentError } from "../../utils/errors";
 
-export default function runCertbot(
-  recordId: string,
+import { RecordInstaceStatus } from "../../types/custom";
+
+export default async function runCertbot(
   instanceId: string,
+  recordId: string,
   subdomain: string,
+  ms = 2000,
+  triesLeft = 50,
 ) {
-  const debug = createDeploymentDebug(Config.CLIENT_OPTIONS.debug);
+  const debug = createCertbotDebug(Config.CLIENT_OPTIONS.debug);
+  const defaultTriesLeft = triesLeft;
 
-  const recordStatusInterval = setInterval(getRecordStatus, 2000);
-
-  async function getRecordStatus() {
-    let recordStatus;
-    let instanceState;
-
-    try {
-      const instanceChangeInfo = await describeInstanceIp(instanceId);
-
-      recordStatus = await describeRecord(recordId);
-      instanceState = instanceChangeInfo?.instanceStateName;
-
-      debug(
-        `Checking recordStatus - [${recordStatus}]`,
-        `Checking instanceState - [${instanceState}]`,
+  return new Promise<RecordInstaceStatus>((resolve, reject) => {
+    const recordStatusInterval = setInterval(async () => {
+      const recordInstaceStatus = await getRecordInstanceStatus(
+        instanceId,
+        recordId,
+        subdomain,
       );
+      const { recordStatus, instanceState } = recordInstaceStatus;
 
-      debug(
-        `Still waiting for requesting a certificate to enable HTTPS on ${subdomain}.${Config.SERVER_URL}...`,
-      );
+      const runCertbotCommandsDelay = async (ms: number) => {
+        return new Promise<void>(resolve =>
+          setTimeout(
+            () => resolve(runCertbotCommands(instanceId, subdomain)),
+            ms,
+          ),
+        );
+      };
 
       if (
         recordStatus === "INSYNC" &&
         instanceState === InstanceStateName.running
       ) {
         clearInterval(recordStatusInterval);
+        await runCertbotCommandsDelay(120000);
 
-        setTimeout(() => runCertbotCommands(instanceId, subdomain), 120000);
+        resolve(recordInstaceStatus);
+      } else if (triesLeft <= 1) {
+        clearInterval(recordStatusInterval);
+        debug(
+          `Error: 'recordStatusInterval' attempted more than ${defaultTriesLeft} times, but didn't work as expected`,
+        );
 
-        return recordStatus;
+        reject(
+          new DeploymentError({
+            code: "runCertbot_recordStatusInterval",
+            message: `'recordStatusInterval' attempted more than ${defaultTriesLeft} times, but didn't work as expected`,
+          }),
+        );
       }
-    } catch (err) {
-      debug(`Error: 'recordStatus' is expected to be a string - ${err}`);
-      throw new DeploymentError({
-        code: "route53Client_GetChangeCommand",
-        message: "recordStatus is typeof undefined",
-      });
-    }
-  }
+
+      triesLeft--;
+    }, ms);
+  });
 }
