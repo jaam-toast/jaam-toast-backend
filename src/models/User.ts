05671c3aadd7556b19d7e7896b495b1252a3e2c0
Taewan Lim <taewan.seoul@gmail.com>
2022-11-20 19:18:07 +0900

    ✨ feat: modify User model and add Repo model

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,17 +2,25 @@ import mongoose from "mongoose";
 import joi from "joi";
 import joigoose from "joigoose";
 
+import { User } from "../types/custom";
+
 const Joigoose = joigoose(mongoose);
 
 const joiUserSchema = joi.object({
   username: joi.string().required(),
   userGithubUri: joi.string().required(),
   userImage: joi.string(),
+  githubAccessToken: joi.string(),
+  myRepos: joi.array().items(
+    joi.string().meta({
+      _mongoose: { type: "ObjectId", ref: "Repo" },
+    }),
+  ),
 });
 
 const userSchema = new mongoose.Schema(Joigoose.convert(joiUserSchema), {
   versionKey: false,
 });
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model<User>("User", userSchema);
 
 export { User, joiUserSchema };
