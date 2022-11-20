705e42c4e1776f9241ec5d12f867f51bb45cc7f7
Taewan Lim <taewan.seoul@gmail.com>
2022-11-20 19:13:09 +0900

    ♻️ refactor: separate getRecordInstanceStatus build-utils function

---
+++ src/deploy/build-utils/getRecordInstanceStatus.ts
@@ -0,0 +1,46 @@
+import Config from "../../config";
+import describeInstanceIp from "../aws/ec2_describeinstances";
+import describeRecord from "../aws/route53_describerecord";
+
+import { createCertbotDebug } from "../../utils/createDebug";
+import { DeploymentError } from "../../utils/errors";
+
+export default async function getRecordInstanceStatus(
+  instanceId: string,
+  recordId: string,
+  subdomain: string,
+) {
+  const debug = createCertbotDebug(Config.CLIENT_OPTIONS.debug);
+
+  let recordStatus;
+  let instanceState;
+
+  try {
+    const instanceChangeInfo = await describeInstanceIp(instanceId);
+
+    recordStatus = await describeRecord(recordId);
+    instanceState = instanceChangeInfo?.instanceStateName;
+
+    debug(
+      `recordStatus: [${recordStatus}]`,
+      `instanceState: [${instanceState}]`,
+    );
+
+    debug(
+      `Waiting before requesting a certificate to enable HTTPS on ${subdomain}.${Config.SERVER_URL}...`,
+    );
+
+    const runCertbotResponse = {
+      recordStatus,
+      instanceState,
+    };
+
+    return runCertbotResponse;
+  } catch (err) {
+    debug(`Error: 'recordStatus' is expected to be a string - ${err}`);
+    throw new DeploymentError({
+      code: "route53Client_GetChangeCommand",
+      message: "recordStatus is typeof undefined",
+    });
+  }
+}
