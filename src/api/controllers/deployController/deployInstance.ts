ae940cbd1189913664bf9f4ce1cb0293453d6fb6
Taewan Lim <taewan.seoul@gmail.com>
2022-11-20 19:01:55 +0900

    ♻️ refactor: create deployInstance controller

---
+++ src/api/controllers/deployController/deployInstance.ts
@@ -0,0 +1,65 @@
+import createError from "http-errors";
+
+import createDeploymentInstance from "../../../deploy/build-utils/createDeploymentInstance";
+import { bulidingLogSocket } from "../../../deploy/socket";
+import { createRepoWebhook } from "../../github/client";
+
+import catchAsync from "../../../utils/asyncHandler";
+
+const deployInstance = catchAsync(async (req, res, next) => {
+  const { githubAccessToken } = req.query;
+
+  if (!githubAccessToken) {
+    return next(createError(400));
+  }
+
+  const {
+    repoName,
+    repoCloneUrl,
+    repoUpdatedAt,
+    nodeVersion,
+    installCommand,
+    buildCommand,
+    envList,
+    bulidType,
+  } = req.body;
+
+  if (!repoName || !repoCloneUrl) {
+    return next(createError(400));
+  }
+
+  const repoOwner = repoCloneUrl.split("https://github.com/")[1].split("/")[0];
+  const repoBuildOptions = {
+    repoOwner,
+    repoName,
+    repoCloneUrl,
+    repoUpdatedAt,
+    nodeVersion,
+    installCommand,
+    buildCommand,
+    envList,
+    bulidType,
+  };
+
+  bulidingLogSocket();
+
+  const { deployedUrl, instanceId } = await createDeploymentInstance(
+    repoBuildOptions,
+  );
+
+  if (!deployedUrl || !instanceId) {
+    return next(createError(401));
+  }
+
+  createRepoWebhook(githubAccessToken as string, repoOwner, repoName);
+
+  req.deploymentData = {
+    ...repoBuildOptions,
+    instanceId,
+    deployedUrl,
+  };
+
+  next();
+});
+
+export default deployInstance;
