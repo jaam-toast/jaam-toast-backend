29508c1ff46016889bd7fdf84924e79a3d787dc1
Taewan Lim <taewan.seoul@gmail.com>
2022-11-20 19:18:07 +0900

    ✨ feat: modify User model and add Repo model

---
+++ src/models/Repo.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import joi from "joi";
+import joigoose from "joigoose";
+
+import { DeploymentData } from "../types/custom";
+
+const Joigoose = joigoose(mongoose);
+
+const joiRepoSchema = joi.object({
+  repoName: joi.string(),
+  repoOwner: joi.string(),
+  repoCloneUrl: joi.string(),
+  repoUpdatedAt: joi.string(),
+  nodeVersion: joi.string(),
+  installCommand: joi.string().allow("").default(""),
+  buildCommand: joi.string().allow("").default(""),
+  bulidType: joi.string().allow("").default(""),
+  envList: joi.array().items(
+    joi.object({
+      key: joi.string(),
+      value: joi.string(),
+    }),
+  ),
+  instanceId: joi.string().allow(""),
+  buildingLog: joi.array().items(joi.string()),
+  deployedUrl: joi.string().allow(""),
+  lastCommitMessage: joi.string(),
+});
+
+const repoSchema = new mongoose.Schema(Joigoose.convert(joiRepoSchema), {
+  versionKey: false,
+});
+const Repo = mongoose.model<DeploymentData>("Repo", repoSchema);
+
+export { Repo, joiRepoSchema };
