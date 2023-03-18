5cd8e456e0e14b24981d45b553b529e1b6a007b3
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-03-18 10:27:36 +0900

    🔥 chore: delete unused files

--- src/services/deploy/aws/libs/ec2Client.ts
+++
@@ -1,9 +0,0 @@
-import { EC2Client } from "@aws-sdk/client-ec2";
-
-import Config from "../../../../config";
-
-const REGION = Config.INSTANCE_REGION;
-
-const ec2Client = new EC2Client({ region: REGION });
-
-export default ec2Client;
