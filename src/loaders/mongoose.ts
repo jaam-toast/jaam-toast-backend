48b8cdb20f076ab6d523a43806ee60e07d0b2bca
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-03-25 16:56:01 +0900

    ✨ feat: add strictQuery to mongoose

--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -5,6 +5,8 @@ import log from "@src/services/Logger";
 
 const mongooseLoader = async (): Promise<void> => {
   try {
+    mongoose.set("strictQuery", false);
+
     await mongoose.connect(Config.DATABASE_URL);
 
     log.debug("🌱 MongoDB connected!");
