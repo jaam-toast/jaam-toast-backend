91d014e6b673cb41f40f884bb1d7c2d18d9c8b50
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-03-25 19:42:04 +0900

    ♻️ refactor: update db handler

--- src/services/ProjectService/handlers/updateProject.ts
+++ src/services/ProjectService/handlers/updateProject.ts
@@ -1,32 +1,67 @@
-import RepoModel from "@src/models/Repo";
-
+import DB from "@src/services/DBService";
 import ProjectService from "@src/services/ProjectService";
 
-async function updateProject(
+const updateProject = async (
   service: ProjectService,
   next: Function,
-): Promise<void> {
-  const { repoCloneUrl, lastCommitMessage } = service;
+): Promise<void> => {
+  const {
+    projectUpdatedAt,
+    projectName,
+    nodeVersion,
+    installCommand,
+    buildCommand,
+    envList,
+    buildType,
+    webhookId,
+    lastCommitMessage,
+    lastCommitHash,
+    instanceId,
+    deployedUrl,
+    deployments,
+    publicIpAddress,
+  } = service;
+
+  const updateData = {
+    ...(webhookId && { webhookId }),
+    ...(projectUpdatedAt && { projectUpdatedAt }),
+    ...(nodeVersion && { nodeVersion }),
+    ...(installCommand && { installCommand }),
+    ...(buildCommand && { buildCommand }),
+    ...(buildType && { buildType }),
+    ...(envList && { envList }),
+    ...(instanceId && { instanceId }),
+    ...(deployedUrl && { deployedUrl }),
+    ...(lastCommitMessage && { lastCommitMessage }),
+    ...(lastCommitHash && { lastCommitHash }),
+    ...(webhookId && { webhookId }),
+    ...(publicIpAddress && { publicIpAddress }),
+    ...(deployments && { deployments }),
+  };
+
+  if (!Object.keys(updateData).length) {
+    service.throw("Cannot find environment data before saving project.");
+  }
 
   try {
-    const userRepo = await RepoModel.findOne({
-      repoCloneUrl,
-    }).lean();
+    const deployment = await DB.Deployment.create();
+    updateData.deployments = [deployment._id];
 
-    service.instanceId = userRepo?.instanceId;
-
-    await RepoModel.updateOne(
-      { repoCloneUrl },
-      { $set: { lastCommitMessage } },
+    const project = await DB.Project.findOneAndUpdate(
+      { projectName },
+      updateData,
     );
+
+    if (!deployment || !project) {
+      service.throw("Failed to update database.");
+    }
+
+    service.projectId = project._id;
   } catch (error) {
-    service.throw(
-      "An unexpected error occurred during updating project.",
-      error,
-    );
+    service.throw("An unexpected error occurred during saving project.", error);
   }
 
   next();
-}
+};
 
 export default updateProject;
