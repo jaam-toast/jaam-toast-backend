8aea1f9cd18450ad551344910df6603217c2c09c
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-03-25 19:39:47 +0900

    ♻️ refactor: update webhook and add last commit hash save

--- src/services/ProjectService/handlers/setGithubInfo.ts
+++ src/services/ProjectService/handlers/setGithubInfo.ts
@@ -1,27 +1,26 @@
 import GithubClient from "@src/services/GithubClient";
-
 import ProjectService from "@src/services/ProjectService";
 
 const setGithubInfo = async (
   service: ProjectService,
   next: Function,
 ): Promise<void> => {
-  const { githubAccessToken, repoName, repoCloneUrl } = service;
+  const { githubAccessToken, space, repoName } = service;
 
-  if (!repoCloneUrl || !repoName) {
+  if (!githubAccessToken || !space || !repoName) {
     service.throw("Cannot find environment data before setting github info.");
   }
 
   try {
     const githubClient = new GithubClient(githubAccessToken as string);
-    const repoOwner = repoCloneUrl
-      .split("https://github.com/")[1]
-      .split("/")[0];
-    const commitList = await githubClient.getCommits(repoOwner, repoName);
-    const lastCommitMessage = commitList[0].commit.message;
 
-    service.repoOwner = repoOwner;
+    const commitList = (await githubClient.getCommits(space, repoName)) as any;
+
+    const lastCommitMessage = commitList[0].commit.message;
+    const lastCommitHash = commitList[0].sha;
+
     service.lastCommitMessage = lastCommitMessage;
+    service.lastCommitHash = lastCommitHash;
   } catch (error) {
     service.throw(
       "An unexpected error occurred during setting github infomations.",
