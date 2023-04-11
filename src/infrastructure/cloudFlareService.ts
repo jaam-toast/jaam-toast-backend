32e1057b01e1bc1b276c100cff8f8a484524d209
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-04-11 18:46:38 +0900

    ✨ feat: create cloudflare service api

---
+++ src/infrastructure/cloudFlareService.ts
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+type Options = {
+  accountId: string;
+  apiKey: string;
+  authEmail: string;
+};
+
+export class CloudFlareService {
+  api;
+
+  constructor({ accountId, apiKey, authEmail }: Options) {
+    this.api = axios.create({
+      baseURL: `https://api.cloudflare.com/client/v4/accounts/${accountId}/pages/projects`,
+      timeout: 8000,
+      headers: {
+        ["X-Auth-Key"]: apiKey,
+        ["X-Auth-Email"]: authEmail,
+      },
+    });
+  }
+
+  async createProject({ projectName }: { projectName: string }) {
+    try {
+      const { data } = await this.api.post("/", {
+        latest_deployment: null,
+        name: projectName,
+        production_branch: "main",
+      });
+
+      return data;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  async deleteProject({ projectName }: { projectName: string }) {
+    try {
+      const { data } = await this.api.delete(`/${projectName}`);
+
+      return data;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+}
