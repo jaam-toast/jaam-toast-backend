8632fbf85609b7921009b4ec8b23959274de2ce3
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-04-11 19:45:56 +0900

    ✨ feat: create domains config, constant and types

---
+++ src/domains/types/index.ts
@@ -0,0 +1,16 @@
+// 수정중..
+export enum Framework {
+  CreateReactApp = "Create React App",
+  ReactStatic = "React Static",
+  NextJs = "Next.js (Static HTML Export)",
+  NuxtJs = "Nuxt.js",
+  Angular = "Angular (Angular CLI)",
+  Astro = "Astro",
+  Gatsby = "Gatsby",
+  GitBook = "GitBook",
+  Jekyll = "Jekyll",
+  Remix = "Remix",
+  Svelte = "Svelte",
+  Vue = "Vue",
+  VuePress = "VuePress",
+}
