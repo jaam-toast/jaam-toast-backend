94d833b1774398c2d83ed1605e98949c4de02aba
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-04-12 16:47:56 +0900

    ♻️ refactor: separate socket files

--- src/infrastructure/socket.ts
+++ src/infrastructure/socket.ts
@@ -1,66 +1,34 @@
 import { Server } from "http";
-import { Server as socketServer, Socket } from "socket.io";
+import { Server as socketServer } from "socket.io";
 
 import { Logger as log } from "@src/common/Logger";
 
-import type { DefaultEventsMap } from "socket.io/dist/typed-events";
-import { BUILD_COMPLETE_MESSAGE } from "@src/common/constants";
+type Options = {
+  server: Server;
+  clientOrigin: string;
+};
 
-class SocketSingleton extends socketServer {
-  private static instance: socketServer<
-    DefaultEventsMap,
-    DefaultEventsMap,
-    DefaultEventsMap,
-    any
-  > | null = null;
+export class SocketSingleton extends socketServer {
+  private static instance: socketServer | null = null;
 
-  constructor(server: Server) {
+  constructor({ server, clientOrigin }: Options) {
     super();
 
     if (SocketSingleton.instance) {
       log.serverError("An attempt was made to create more than one socket.");
+
       throw new Error("You can only create one instance!");
     }
 
     SocketSingleton.instance = new socketServer(server, {
       cors: {
-        origin: Config.CLIENT_URL,
+        origin: clientOrigin,
         methods: ["GET", "POST"],
       },
     });
-
-    log.debug("🚀 A new socket instance is created");
   }
 
-  init() {
-    if (!SocketSingleton.instance) {
-      return;
-    }
-
-    SocketSingleton.instance.on("connection", (socket: Socket) => {
-      socket.on("connection", () => {
-        log.debug(
-          "Socket is connected in order to send back to client the newly created deployment building log",
-        );
-      });
-
-      socket.on("get-building-log", repoName => {
-        log.debug(`Getting ready for sending a building log for ${repoName}`);
-
-        log.subscribe((message: string) => {
-          if (message === BUILD_COMPLETE_MESSAGE) {
-            socket.emit("build-complete", message);
-          }
-
-          socket.emit("new-building-log", message);
-        });
-      });
-    });
+  getInstance() {
+    return SocketSingleton.instance;
   }
 }
-
-export const createSocket = async (server: Server): Promise<void> => {
-  const socket = new SocketSingleton(server);
-
-  socket.init();
-};
