4063a639b9897a08fbca19480b0623769d604874
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-04-12 21:27:21 +0900

    ♻️ refactor: update express type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,18 +1,1 @@
-import { User } from "./db";
-
-declare global {
-  namespace Express {
-    export interface Request {
-      user: User;
-    }
-  }
-
-  namespace NodeJS {
-    interface ProcessEnv {
-      MONGODB_URI: string;
-      JWT_SECRET_KEY: string;
-      GITHUB_CLIENT_ID: string;
-      GITHUB_CLIENT_SECRET: string;
-    }
-  }
-}
+declare module "Express";
