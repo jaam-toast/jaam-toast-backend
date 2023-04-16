1fadd4dfaa5f4d45220b10da8e9edf654cdb2ef2
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-04-17 06:57:05 +0900

    ♻️ refactor: change project route to project service

--- src/app/routes/projects.ts
+++ src/app/routes/projects.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import createError from "http-errors";
 import Joi from "joi";
+import * as _ from "lodash";
 
-import { BuildService } from "../../domains/buildService";
-import { CmsService } from "../../domains/cmsService";
-import { ProjectRepository } from "../../domains/repositories/projectRepository";
-import { UserRepository } from "../../domains/repositories/userRepository";
 import verifyToken from "../middlewares/verifyToken";
-import validateSchema from "../middlewares/validateSchema";
+import validateRequest from "../middlewares/validateRequest";
 import { asyncHandler } from "../utils/asyncHandler";
+import { BUILD_MESSAGE } from "../../config/constants";
+import { ProjectService } from "../../domains/projectService";
+import { UserService } from "../../domains/userService";
+import { Logger as log } from "../../util/Logger";
+import { container } from "../../domains/@config/di.config";
 
 const route = Router();
 
@@ -18,91 +20,58 @@ const projectsRouter = (app: Router) => {
   route.post(
     "/",
     verifyToken,
-    asyncHandler(async (req, res, next) => {
+    validateRequest(
+      Joi.object({
+        userId: Joi.string(),
+        space: Joi.string().required(),
+        repoName: Joi.string().required(),
+        repoCloneUrl: Joi.string().required(),
+        projectName: Joi.string().required(),
+        projectUpdatedAt: Joi.string().required(),
+        framework: Joi.string().required(),
+        installCommand: Joi.string().required(),
+        buildCommand: Joi.string().required(),
+        envList: Joi.array().required(),
+      }),
+      "body",
+    ),
+    asyncHandler(async (req, res) => {
       const projectOptions = req.body;
-
-      const {
-        userId,
-        space,
-        repoName,
-        repoCloneUrl,
-        projectName,
-        projectUpdatedAt,
-        framework,
-        installCommand,
-        buildCommand,
-        envList,
-      } = projectOptions;
-
-      if (
-        !userId ||
-        !space ||
-        !repoName ||
-        !repoCloneUrl ||
-        !projectName ||
-        !projectUpdatedAt ||
-        !framework ||
-        !installCommand ||
-        !buildCommand ||
-        !envList
-      ) {
-        return next(
-          createError(401, "Cannot find environment data 'project_name'"),
-        );
-      }
-
-      const project = await ProjectRepository.create(projectOptions);
-      const user = await UserRepository.findByIdAndUpdateProject(
-        userId,
-        project._id,
-      );
-
-      if (!project || !user) {
-        return next(createError(500, "Failed to create database."));
-      }
+      const { username } = req.app.locals;
+      const { projectName, userId } = projectOptions;
 
       res.json({
         message: "ok",
-        result: project._id,
+        result: projectName,
       });
 
-      const buildUrl = await BuildService.createBuild({
-        repoName,
-        repoCloneUrl,
-        projectName,
-        framework,
-        installCommand,
-        buildCommand,
-        envList,
-      });
-      const cmsUrl = await CmsService.createApi();
-      await ProjectRepository.findByIdAndUpdate(project._id, {
-        buildUrl,
-        cmsUrl,
-      });
+      const projectService = container.get<ProjectService>("ProjectService");
+      const userService = container.get<UserService>("UserService");
+
+      try {
+        await projectService.createProject(_.omit(projectOptions, ["userId"]));
+        await userService.addProject({ userId, projectName });
+      } catch (error) {
+        console.log(error);
+        log.serverError(BUILD_MESSAGE.ERROR.FAIL_PROJECT_CREATION);
+      }
     }),
   );
 
-  // 진행중..
   route.get(
     "/:project_name",
-    validateSchema(
-      Joi.object({
-        project_name: Joi.string().regex(/^[a-zA-Z0-9]+$/i),
-      }),
-      "params[project_name]",
-    ),
     verifyToken,
+    validateRequest(
+      Joi.object({
+        project_name: Joi.string().required(),
+      }),
+      "params ",
+    ),
     asyncHandler(async (req, res, next) => {
-      const { projectId } = req.params;
+      const { project_name } = req.params;
 
-      if (!projectId) {
-        return next(
-          createError(401, "Cannot find environment data 'project_name'"),
-        );
-      }
-
-      const project = await ProjectRepository.findById(projectId);
+      const projectService = container.get<ProjectService>("ProjectService");
+      const project = await projectService.getByProjectName(project_name);
 
       if (!project) {
         return next(createError(400, "Project data does not exist."));
@@ -117,56 +86,41 @@ const projectsRouter = (app: Router) => {
 
   route.put(
     "/:project_name",
-    validateSchema(
-      Joi.object({
-        project_name: Joi.string().regex(/^[a-zA-Z0-9]+$/i),
-      }),
-      "params[project_name]",
-    ),
     verifyToken,
-    asyncHandler(async (req, res, next) => {
-      const { projectId } = req.params;
+    validateRequest(
+      Joi.object({
+        project_name: Joi.string().required(),
+      }),
+      "params",
+    ),
+    asyncHandler(async (req, res) => {
+      const { project_name } = req.params;
       const updateData = req.body;
-
-      if (!projectId) {
-        return next(createError(500, "Failed to update database."));
-      }
-
-      const buildService = new BuildService();
-      const projectData = await buildService.updateBuild(updateData);
 
       return res.json({
         message: "ok",
-        result: projectData,
       });
     }),
   );
 
   route.delete(
     "/:project_name",
-    validateSchema(
-      Joi.object({
-        project_name: Joi.string().regex(/^[a-zA-Z0-9]+$/i),
-      }),
-      "params[project_name]",
-    ),
     verifyToken,
-    asyncHandler(async (req, res, next) => {
-      const { projectId } = req.params;
+    validateRequest(
+      Joi.object({
+        project_name: Joi.string().required(),
+      }),
+      "params",
+    ),
+    asyncHandler(async (req, res) => {
+      const { username } = req.app.locals;
+      const { project_name: projectName } = req.params;
 
-      if (!projectId) {
-        return next(
-          createError(
-            400,
-            "Cannot find environment data 'githubAccessToken', and 'projectName'",
-          ),
-        );
-      }
+      const projectService = container.get<ProjectService>("ProjectService");
+      const userService = container.get<UserService>("UserService");
 
-      const buildService = new BuildService();
-      const cmsService = new CmsService();
-      await buildService.deleteBuild(projectId);
-      await cmsService.deleteApi();
+      await projectService.deleteProject({ projectName });
+      await userService.deleteProject({ username, projectName });
 
       return res.status(204).json({
         message: "ok",
