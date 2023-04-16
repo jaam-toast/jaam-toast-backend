a1f8f8373168b8f185d9a3ab6a6af491dd1b9cae
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-04-17 06:38:09 +0900

    ♻️ refactor: apply joi validator

--- src/app/routes/login.ts
+++ src/app/routes/login.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import createError from "http-errors";
 import jwt from "jsonwebtoken";
 
-import { UserRepository } from "../../domains/repositories/userRepository";
 import verifyGithubCode from "../middlewares/verifyGithubCode";
 import { asyncHandler } from "../utils/asyncHandler";
 import Config from "../../config";
+import validateRequest from "../middlewares/validateRequest";
+import Joi from "joi";
+import { container } from "src/domains/@config/inversify.config";
+import { UserService } from "../../domains/userService";
 
 const route = Router();
 
@@ -15,24 +17,26 @@ const loginRouter = (app: Router) => {
   route.get(
     "/",
     verifyGithubCode,
+    validateRequest(
+      Joi.object({
+        username: Joi.string().required(),
+        userGithubUri: Joi.string().required(),
+        userImage: Joi.string().required(),
+        githubAccessToken: Joi.string().required(),
+      }),
+      "app[locals]",
+    ),
     asyncHandler(async (req, res, next) => {
       const { username, userGithubUri, userImage, githubAccessToken } =
         req.app.locals;
 
-      if (!username || !userGithubUri) {
-        return next(createError(401));
-      }
-
-      let userData = await UserRepository.findOne({ userGithubUri });
-
-      if (!userData) {
-        userData = await UserRepository.create({
-          username,
-          userGithubUri,
-          userImage,
-          githubAccessToken,
-        });
-      }
+      const userService = container.get<UserService>("UserService");
+      const userData = await userService.login({
+        username,
+        userGithubUri,
+        userImage,
+        githubAccessToken,
+      });
 
       const userPayload = {
         username: userData?.username,
@@ -44,6 +48,7 @@ const loginRouter = (app: Router) => {
         expiresIn: "1d",
       });
 
+      // TODO - cookie 수정
       const { referer } = req.headers;
       const loginData = JSON.stringify({
         id: userData?._id,
@@ -57,7 +62,6 @@ const loginRouter = (app: Router) => {
       return res
         .cookie("loginData", loginData, {
           maxAge: 24 * 60 * 60 * 1000,
-          httpOnly: true,
         })
         .redirect(referer ?? Config.CLIENT_URL);
     }),
