6e25c558b33a6200b8560121d7f7f19f09957711
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-04-17 02:24:49 +0900

    ♻️ refactor: create constants message

---
+++ src/config/constants.ts
@@ -0,0 +1,21 @@
+export const BUILD_TASK_MESSAGE = {};
+
+export const BUILD_MESSAGE = {
+  WORKING_ON_BUILD_PROJECT:
+    "Working on a build using user project resources...",
+  COMPLETE_RESOURCE_CREATE: "User project resource creation complete",
+  COMPLETE_INSTALL_DEPENDENCIES:
+    "Finish installing the necessary files for your project",
+  CHILD_PROCESS_EXITED_WITH_CODE: "childProcess exited with code >",
+  COMPLETE: "A new project's data is created successfully!",
+
+  ERROR: {
+    FAIL_PROJECT_CREATION: "Project creation failed.",
+    FAIL_RESOURCE_CREATION: "Cannot find build Resource",
+    ENVIRONMENT_DATA_NOT_FOUND:
+      "Cannot find environment data before create project",
+    DOMAIN_CREATE_FAIL: "Domain creation failed",
+    UNEXPECTED_DURING_BUILD:
+      "An unexpected error occurred during build project",
+  },
+};
