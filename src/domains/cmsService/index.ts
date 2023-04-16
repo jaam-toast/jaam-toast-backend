95c44767f16c33ecf26eb129913e55031afa2ede
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-04-17 06:58:36 +0900

    ✨ feat: create ability to create api domain

--- src/domains/cmsService/index.ts
+++ src/domains/cmsService/index.ts
@@ -1,7 +1,36 @@
-export class CmsService {
-  static async createApi() {
-    return "cmsUrl";
+import { injectable } from "inversify";
+
+import { createDomain } from "./createDomain";
+import { createToken } from "./createToken";
+import { deleteDomain } from "./deleteDomain";
+
+export interface ICmsService {
+  createApi({
+    projectName,
+  }: {
+    projectName: string;
+  }): Promise<{ cmsDomain: string; cmsToken: string }>;
+  updateApi(): Promise<void>;
+  deleteApi({ subdomain }: { subdomain: string }): Promise<void>;
+}
+
+@injectable()
+export class CmsService implements ICmsService {
+  async createApi({ projectName }: any) {
+    try {
+      const cmsDomain = await createDomain({ subdomain: `api-${projectName}` });
+
+      const cmsToken = createToken({ payload: projectName });
+
+      return { cmsDomain, cmsToken };
+    } catch (error) {
+      throw error;
+    }
   }
+
   async updateApi() {}
-  async deleteApi() {}
+
+  async deleteApi({ subdomain }: { subdomain: string }) {
+    await deleteDomain({ subdomain });
+  }
 }
