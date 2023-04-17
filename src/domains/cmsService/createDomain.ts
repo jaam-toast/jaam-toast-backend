115de86c4e1047dd0370160569c162bcf53d5e7b
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-04-17 21:59:25 +0900

    🔥 chore: apply type

--- src/domains/cmsService/createDomain.ts
+++ src/domains/cmsService/createDomain.ts
@@ -3,7 +3,11 @@ import { Logger as log } from "../../utils/Logger";
 import Config from "../../config";
 import { CMS_MESSAGE } from "../../config/constants";
 
-export async function createDomain({ subdomain }: any): Promise<string> {
+export async function createDomain({
+  subdomain,
+}: {
+  subdomain: string;
+}): Promise<string> {
   const route53 = new Route53();
   const recordChangeInfo = await route53.createARecord({
     recordName: `${subdomain}.${Config.SERVER_URL}`,
