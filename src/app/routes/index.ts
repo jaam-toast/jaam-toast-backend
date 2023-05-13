41b7c20ec07f675a654667ef99fd609dc6f9dbaf
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-05-13 18:47:20 +0900

    🐛 fix: debug errors

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { usersRouter } from "./routeUsers";
 import { loginRouter } from "./routeLogin";
 import { projectsRouter } from "./routeProjects";
+import { projectOptionsRouter } from "./routeProjectOptions";
 import { schemasRouter } from "./routeSchemas";
 import { storageRouter } from "./routeStorage";
 
@@ -11,5 +12,6 @@ export const router = Router();
 router.use(usersRouter);
 router.use(loginRouter);
 router.use(projectsRouter);
+router.use(projectOptionsRouter);
 router.use(schemasRouter);
 router.use(storageRouter);
