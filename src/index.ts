55c82c32ae8df9e09253fc157baed604c89adf73
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-05-24 22:24:36 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { createApp } from "./app/createApp";
 import { connectDatabase } from "./app/connectDatabase";
 import { createServer } from "./app/createServer";
 import { createSocket } from "./app/createSocket";
+import { emitEvent } from "./@utils/emitEvent";
 
 async function startServer(): Promise<void> {
   const app = await createApp();
@@ -12,6 +13,10 @@ async function startServer(): Promise<void> {
 
   await connectDatabase();
   await createSocket({ server });
+
+  emitEvent("DEPLOYMENT_UPDATED", {
+    projectName: "test",
+  });
 }
 
 startServer();
